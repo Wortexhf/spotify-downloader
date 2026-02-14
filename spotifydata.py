c43ab01b8832be9e2cc2bc99047bdf5fa5947567
wortexhf <wortexhf@gmail.com>
2026-02-14 18:51:33 +0100

    first commit

---
+++ spotifydata.py
@@ -0,0 +1,15 @@
+import spotipy
+track = None
+link = input("Enter the Spotify playlist link: ")
+
+if link = "https://open.spotify.com/track/":
+    link = track
+else:
+    if link = "https://open.spotify.com/playlist/":
+        link = track
+
+if album:
+    print(f"list of songs in the album: {tracks} ")
+
+tracks = 
+
